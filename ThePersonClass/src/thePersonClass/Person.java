a96c43b948d02e74a2e78d253a933c07e48a1f40
Michal Sitarz <me@michalsitarz.com>
2017-11-06 16:02:29 +0000

    Person class: fields, ctor, SETters, GETters

--- ThePersonClass/src/thePersonClass/Person.java
+++ ThePersonClass/src/thePersonClass/Person.java
@@ -9,5 +9,88 @@ package thePersonClass;
  * @author MJSitarz
  */
 public class Person {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+    private int height;
+    private int weight;
+    private char gender;
+    private Person spouse;
+
+    //ctor
+    public Person(String _firstName, String _lastName, int _age, int _height, int _weight, char _gender) {
+        firstName = _firstName;
+        lastName = _lastName;
+        age = _age;
+        height = _height;
+        weight = _weight;
+        gender = _gender;
+    }
     
+    
+    //SETters
+    
+    public void setFirstName(String _firstName) {
+        firstName = _firstName;
+    }
+
+    public void setLastName(String _lastName) {
+        lastName = _lastName;
+    }
+
+    public void setAge(int _age) {
+        age = _age;
+    }
+
+    public void setHeight(int _height) {
+        height = _height;
+    }
+
+    public void setWeight(int _weight) {
+        weight = _weight;
+    }
+    
+    public void setSpouse(Person _spouse) {
+        spouse = _spouse;
+    }
+    
+    // no setter for field: gender - cannot be changed
+    
+    
+    //GETters
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public Person getSpouse() {
+        return spouse;
+    }
+    
+    
+    // other methods
+    
+    
+
 }
