1523d3e45bd0de9d0a0ab9af362925ee167cd234
Michal Sitarz <me@michalsitarz.com>
2017-11-06 16:57:13 +0000

    TO DO list update + test methods

--- ThePersonClass/src/thePersonClass/Main.java
+++ ThePersonClass/src/thePersonClass/Main.java
@@ -5,9 +5,9 @@
  * 2.  [•]
  * 3.  [•]
  * 4.  [ ]
-    a) [ ]
-    b) [ ]
-    c) [ ]
+    a) [•]
+    b) [•]
+    c) [•]
     d) [ ]
     e) [ ]
     f) [ ]
@@ -27,5 +27,45 @@ package thePersonClass;
  * @author MJSitarz@dundee.ac.uk
  */
 public class Main {
+    public static void main(String[] args) {
+        
+        // testing methods
+        test1();
+        test2();
+        test3();
+    }
+    
+    //testing methods
+    
+    // tests getFullName method
+    public static void test1(){
+        System.out.println("\n[ Test: 1 ]\n===========");
+        Person p = new Person("Ted", "Testovny", 99, 200, 75, 'M');
+        System.out.println(p.getFullName());
+    }
+    
+    public static void test2(){
+        System.out.println("\n[ Test: 2 ]\n===========");
+        Person p = new Person("Ted", "Testovny", 99, 200, 75, 'M');
+        
+        System.out.println("> Not married");
+        System.out.println("Expected: false");
+        System.out.println("Result:   "+p.getMartialStatus());
+        
+        System.out.println("> Married");
+        p.setSpouse(p);
+        System.out.println("Expected: true");
+        System.out.println("Result:   "+p.getMartialStatus());
+        
+    }
+    
+    public static void test3(){
+        System.out.println("\n[ Test: 3 ]\n===========");
+        Person p = new Person("Ted", "Testovny", 99, 200, 75, 'M');
+        
+        System.out.println("> BMI");
+        System.out.println("Expected: 18.75");
+        System.out.println("Result:   "+p.getBMIindex());
+    }
     
 }
