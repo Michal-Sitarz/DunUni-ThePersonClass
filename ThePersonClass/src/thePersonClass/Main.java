eabe810ded39adaf3f24937288579a0681a659fa
Michal Sitarz <me@michalsitarz.com>
2017-11-06 16:09:23 +0000

    TO DO list (in the Main class)

--- ThePersonClass/src/thePersonClass/Main.java
+++ ThePersonClass/src/thePersonClass/Main.java
@@ -1,12 +1,30 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * TO DO list:
+ * ==========
+ * 1.  [•]
+ * 2.  [•]
+ * 3.  [•]
+ * 4.  [ ]
+    a) [ ]
+    b) [ ]
+    c) [ ]
+    d) [ ]
+    e) [ ]
+    f) [ ]
+    g) [ ]
+ * 5.  [ ]
+ * 6.  [ ]
+ * 7.  [ ]
+ * 8.  [ ]
+ * 9.  [ ]
+ * 10. [ ]
+ *
  */
+
 package thePersonClass;
 
 /*
- * @author MJSitarz
+ * @author MJSitarz@dundee.ac.uk
  */
 public class Main {
     
