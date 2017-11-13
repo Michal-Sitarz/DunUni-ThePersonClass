06388c4aa0b673ce0036566076cf25604576dfbb
Michal Sitarz <me@michalsitarz.com>
2017-11-13 15:31:54 +0000

    test method: showBMImeaning()

--- ThePersonClass/src/thePersonClass/Main.java
+++ ThePersonClass/src/thePersonClass/Main.java
@@ -8,7 +8,7 @@
     a) [•]
     b) [•]
     c) [•]
-    d) [ ]
+    d) [•]
     e) [ ]
     f) [ ]
     g) [ ]
@@ -20,52 +20,62 @@
  * 10. [ ]
  *
  */
-
 package thePersonClass;
 
 /*
  * @author MJSitarz@dundee.ac.uk
  */
 public class Main {
+
     public static void main(String[] args) {
-        
+
         // testing methods
         test1();
         test2();
         test3();
+        test4();
     }
-    
+
     //testing methods
-    
     // tests getFullName method
-    public static void test1(){
+    public static void test1() {
         System.out.println("\n[ Test: 1 ]\n===========");
         Person p = new Person("Ted", "Testovny", 99, 200, 75, 'M');
         System.out.println(p.getFullName());
     }
-    
-    public static void test2(){
+
+    public static void test2() {
         System.out.println("\n[ Test: 2 ]\n===========");
         Person p = new Person("Ted", "Testovny", 99, 200, 75, 'M');
-        
+
         System.out.println("> Not married");
         System.out.println("Expected: false");
-        System.out.println("Result:   "+p.getMartialStatus());
-        
+        System.out.println("Result:   " + p.getMartialStatus());
+
         System.out.println("> Married");
         p.setSpouse(p);
         System.out.println("Expected: true");
-        System.out.println("Result:   "+p.getMartialStatus());
-        
+        System.out.println("Result:   " + p.getMartialStatus());
+
     }
-    
-    public static void test3(){
+
+    public static void test3() {
         System.out.println("\n[ Test: 3 ]\n===========");
         Person p = new Person("Ted", "Testovny", 99, 200, 75, 'M');
-        
+
         System.out.println("> BMI");
         System.out.println("Expected: 18.75");
-        System.out.println("Result:   "+p.getBMIindex());
+        System.out.println("Result:   " + p.getBMIindex());
     }
-    
+
+    public static void test4() {
+        System.out.println("\n[ Test: 4 ]\n===========");
+        Person p = new Person("Ted", "Testovny", 99, 200, 75, 'M');
+
+        System.out.println("> BMI meaning");
+        System.out.println("Expected: This person has normal weight.");
+        System.out.print("Result:   ");
+        p.showBMImeaning();
+    }
+
 }
