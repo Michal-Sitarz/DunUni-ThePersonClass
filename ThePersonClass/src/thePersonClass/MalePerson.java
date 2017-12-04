8ce873bbb0d2302bbebd183cd178b5be18b83a1c
Michal Sitarz <me@michalsitarz.com>
2017-12-04 16:37:47 +0000

    Inheritance implementation

--- ThePersonClass/src/thePersonClass/MalePerson.java
+++ ThePersonClass/src/thePersonClass/MalePerson.java
@@ -3,6 +3,12 @@ package thePersonClass;
 /**
  * @author MJSitarz
  */
-public class MalePerson {
-    
+public class MalePerson extends Person {
+
+    //ctor
+    public MalePerson(String _firstName, String _lastName, int _age, int _height, int _weight) {
+        super(_firstName, _lastName, _age, _height, _weight);
+
+    }
+
 }
