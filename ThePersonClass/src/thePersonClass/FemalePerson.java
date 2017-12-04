b0f3ad434ccf48271ead165a9268329bff8712c0
Michal Sitarz <me@michalsitarz.com>
2017-12-04 16:37:53 +0000

    Inheritance implementation

--- ThePersonClass/src/thePersonClass/FemalePerson.java
+++ ThePersonClass/src/thePersonClass/FemalePerson.java
@@ -3,6 +3,18 @@ package thePersonClass;
 /**
  * @author MJSitarz
  */
-public class FemalePerson {
-    
+public class FemalePerson extends Person {
+
+    public FemalePerson(String _firstName, String _lastName, int _age, int _height, int _weight) {
+        super(_firstName, _lastName, _age, _height, _weight);
+
+    }
+
+    public Person procreate(String firstName, String lastName, int age, int height, int weight) {
+
+        Person baby = new FemalePerson(firstName, lastName, age, height, weight);
+
+        return baby;
+    }
+
 }
